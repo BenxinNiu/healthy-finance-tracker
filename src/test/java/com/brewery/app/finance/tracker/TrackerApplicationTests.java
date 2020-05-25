636c633e4f886babaf40ddcb23833b2f446ed611
BenxinNiu <benxin.niu@mun.ca>
2020-05-24 20:37:03 -0400

    intial commit

---
+++ src/test/java/com/brewery/app/finance/tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewery.app.finance.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
